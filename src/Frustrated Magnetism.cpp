a2956d4e82213c21c25ea569d4111dd7b2dc75ca
bsplu <ruc.bs.plu@hotmail.com>
2014-03-14 19:19:21 +0800

    完善读取函数：之前存在地址访问错误，现已改正

--- src/Frustrated Magnetism.cpp
+++ src/Frustrated Magnetism.cpp
@@ -51,6 +51,13 @@ public:
 	int leng_s_p_buyim;
 	int leng_p_buyim;
 
+	person(){
+		p_id = 0;
+		leng_s_p_buyim = 0;
+		leng_p_buyim = 0;
+		p_buyimformation = NULL;
+	}
+
 	int get_person_id() {
 		return p_id;
 	}
@@ -59,7 +66,10 @@ public:
 	}
 
 	void allocate_p_buy(int leng){
-		delete [] p_buyimformation;
+		if(p_buyimformation != NULL){
+			delete [] p_buyimformation;
+			p_buyimformation = NULL;
+		}
 		p_buyimformation = new BuyImformation [leng];
 		leng_p_buyim = leng;
 		leng_s_p_buyim = 0;
@@ -67,12 +77,13 @@ public:
 
 	void push_p_buy(int brand_id_input,int type_input,
 			int visit_datetime_month_input,int visit_datetime_day_input){
-		if(leng_s_p_buyim == leng_p_buyim){
+		if(leng_s_p_buyim == leng_p_buyim && leng_s_p_buyim > 0){
 			BuyImformation * p_buyimformation_local = new BuyImformation [leng_p_buyim];
 			for(int i=0;i<leng_p_buyim;i++){
 				p_buyimformation_local[i] = p_buyimformation[i];
 			}
 			delete [] p_buyimformation;
+			p_buyimformation = NULL;
 			p_buyimformation = new BuyImformation [2*leng_p_buyim];
 			for(int i=0;i<leng_p_buyim;i++){
 				p_buyimformation[i] = p_buyimformation_local[i];
@@ -93,11 +104,14 @@ public:
 				p_buyimformation_local[i] = p_buyimformation[i];
 			}
 			delete [] p_buyimformation;
+			p_buyimformation = NULL;
 			p_buyimformation = new BuyImformation [leng_s_p_buyim];
 			for(int i=0;i<leng_s_p_buyim;i++){
 				p_buyimformation[i] = p_buyimformation_local[i];
 			}
 			leng_p_buyim = leng_s_p_buyim;
+			delete []p_buyimformation_local;
+			p_buyimformation_local = NULL;
 		}
 	}
 
@@ -109,6 +123,7 @@ public:
 		leng_s_p_buyim = a.leng_s_p_buyim;
 		if(a.leng_p_buyim >0){
 		delete [] p_buyimformation;
+		p_buyimformation = NULL;
 		p_buyimformation = new BuyImformation [a.leng_p_buyim];
 		for(int i=0;i<leng_s_p_buyim;i++){
 			p_buyimformation[i] = a.p_buyimformation[i];
@@ -119,27 +134,33 @@ public:
 
 	~person() {
 		delete[] p_buyimformation;
+		p_buyimformation = NULL;
 	}
 
 };
 
 int main() {
-	void read_txt(person * arry_person,int & leng_s_arry_person);
+	void read_txt(person* &arry_person, int & leng_s_arry_person);
 
 
-	person * arry_person;
-	int leng_s_arry_person;
+	person * arry_person = NULL;
+	int leng_s_arry_person=0;
 
-	read_txt(arry_person,leng_s_arry_person);
+
+	//cout<<leng_s_arry_person<<endl;
+
+	read_txt( arry_person,leng_s_arry_person);
 
 
 	cout<<leng_s_arry_person<<endl;
 
+	cout<<arry_person[0].p_buyimformation[0].brand_id<<endl;
 
 	return 0;
 }
 
-void read_txt(person * arry_person,int & leng_s_arry_person) {
+void read_txt(person* &arry_person_inpute, int & leng_s_arry_person) {
+
 	FILE* fp;
 
 	if ((fp = fopen("t_alibaba_data.csv", "r")) < 0) {
@@ -150,8 +171,10 @@ void read_txt(person * arry_person,int & leng_s_arry_person) {
 	int i_line = 0;
 	int p_id_pre = -1;
 	int leng_arry_person = 1000;//数组长度
+
 	leng_s_arry_person = 0;//已储存个数
-	arry_person = new person [leng_arry_person];
+	//cout<<"==================="<<endl;
+	person * arry_person = new person [leng_arry_person];
 
 	while (!feof(fp)) {
 
@@ -163,6 +186,7 @@ void read_txt(person * arry_person,int & leng_s_arry_person) {
 				&visit_datetime_day_local);
 
 		i_line++;
+
 		if (p_id_pre == p_id_pre_local) {
 			//继续当前用户
 
@@ -177,7 +201,6 @@ void read_txt(person * arry_person,int & leng_s_arry_person) {
 			//判断是否需要扩容{-----------------------------------------------------
 			if(leng_s_arry_person == leng_arry_person){
 				//数组长度不足需要扩容
-
 				//建立临时数组进行复制
 				person * arry_person_local = new person [leng_arry_person];
 				for(int i=0;i<leng_arry_person;i++){
@@ -187,6 +210,7 @@ void read_txt(person * arry_person,int & leng_s_arry_person) {
 				//将长度扩为原先2倍
 
 				delete [] arry_person;
+				arry_person = NULL;
 				arry_person = new person [leng_arry_person*2];
 
 				for(int i=0;i<leng_arry_person;i++){
@@ -195,13 +219,18 @@ void read_txt(person * arry_person,int & leng_s_arry_person) {
 				leng_arry_person *= 2;
 
 				delete [] arry_person_local;
+				arry_person_local = NULL;
 			}
 			//判断扩容end}=========================================================
 
 			//新用户初始化{---------------------------------------------------------
+
 			p_id_pre = p_id_pre_local;
 			arry_person[leng_s_arry_person].change_person_id(p_id_pre);
+
 			arry_person[leng_s_arry_person].allocate_p_buy(100);
+
+
 			arry_person[leng_s_arry_person].p_buyimformation[0].initialize(
 					brand_id_local,type_local, visit_datetime_month_local,
 					visit_datetime_day_local);
@@ -216,9 +245,23 @@ void read_txt(person * arry_person,int & leng_s_arry_person) {
 			//优化}===============================================================
 
 		}
+		//cout<<arry_person[0].p_buyimformation[0].brand_id<<endl;
 	}
 
 	//读取完毕
 	//cout<<"读取完毕"<<endl;
+	//cout<<arry_person[0].p_buyimformation[0].brand_id<<endl;
+
+	if(arry_person_inpute == NULL){
+		arry_person_inpute = new person [leng_s_arry_person];
+	}else{
+		delete [] arry_person_inpute;
+		arry_person_inpute = NULL;
+		arry_person_inpute = new person [leng_s_arry_person];
+	}
+
+	for(int i=0;i<leng_s_arry_person;i++){
+		arry_person_inpute[i] = arry_person[i];
+	}
 }
 
